8d8c39ba27ed881479f1fa1f85034eb1dd1ba88b
moham <moham@DESKTOP-GT9TAGB>
2019-07-20 18:25:32 +0530

    there is an error

---
+++ Introduction/src/p2/Test2.java
@@ -0,0 +1,14 @@
+package p2;
+
+public class Test2 {
+	public static void main(String[]args) {
+	int men,  women; 
+		men = 18;
+		women = 30;
+				
+				if (men < 15 && women < 30);
+				System.out.println("can be seated ");
+	
+				System.out.println( "cannot be seated" ) ;
+	}
+}
