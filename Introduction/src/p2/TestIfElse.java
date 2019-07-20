25605e99c9de6874dff617afc7d11a5083578f59
moham <moham@DESKTOP-GT9TAGB>
2019-07-20 17:08:07 +0530

    IfElse statement

---
+++ Introduction/src/p2/TestIfElse.java
@@ -0,0 +1,16 @@
+package p2;
+
+public class TestIfElse {
+	public static void main(String[]args) {
+		int test = 6;
+		if (test == 9) {
+		System.out.println("yes");
+	
+		}else ;
+	System.out.println(" this is else ");
+}
+	}
+
+
+
+
